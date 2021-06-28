59275c4187d0a6446904c9beae5491936bba946f
Gustavo-F <fgustavo016@gmail.com>
2021-06-27 22:58:03 -0300

    Removed image field to Game

--- Lista-de-Desejos/app/src/main/java/com/example/listadedesejos/Game.kt
+++ Lista-de-Desejos/app/src/main/java/com/example/listadedesejos/Game.kt
@@ -2,4 +2,4 @@ package com.example.listadedesejos
 
 import java.util.*
 
-data class Game(val image: Int, val name: String, var price: Float = 0.0f, val launchDate: Date?, var developer: String)
+data class Game(val name: String, var price: Float = 0.0f, val launchDate: Date?, var developer: String)
