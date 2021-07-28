b8f1cb9be9f4fdc58346c6434fbc15121d55e13a
Gustavo-F <fgustavo016@gmail.com>
2021-07-27 21:50:51 -0300

    Criados fragmentos para heads tails e rolldice

---
+++ AppGames/app/src/main/java/com/gustavo/appgames/HeadsTailsFragment.kt
@@ -0,0 +1,11 @@
+package com.gustavo.appgames
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class HeadsTailsFragment: Fragment(R.layout.fragment_heads_tails) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ AppGames/app/src/main/java/com/gustavo/appgames/RollDiceFragment.kt
@@ -0,0 +1,11 @@
+package com.gustavo.appgames
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class RollDiceFragment: Fragment(R.layout.fragment_rolldice){
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
