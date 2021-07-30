031195879ce26d7ca55b81a1253d0bef7fe83b7e
Gustavo-F <fgustavo016@gmail.com>
2021-07-29 23:37:36 -0300

    Implementaçãp dp jogo de cara e coroa

--- AppGames/app/src/main/java/com/gustavo/appgames/HeadsTailsFragment.kt
+++ AppGames/app/src/main/java/com/gustavo/appgames/HeadsTailsFragment.kt
@@ -1,9 +1,13 @@
 package com.gustavo.appgames
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.gustavo.appgames.databinding.FragmentHeadsTailsBinding
 
@@ -11,12 +15,13 @@ class HeadsTailsFragment: Fragment(R.layout.fragment_heads_tails) {
 
     private var _binding: FragmentHeadsTailsBinding? = null
     private val binding get() = _binding!!
+    private var selectedFace: Int = -1
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentHeadsTailsBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -29,12 +34,42 @@ class HeadsTailsFragment: Fragment(R.layout.fragment_heads_tails) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.addWinButton.setOnClickListener {
-            Data.loggedUser?.winsAndDefeats!![0].wins += 1
+        binding.selectHeadButton.setOnClickListener {
+            selectedFace = 0
+            setCoinFace()
         }
 
-        binding.addDefeatButton.setOnClickListener {
-            Data.loggedUser?.winsAndDefeats!![0].defeats += 1
+        binding.selectTailButton.setOnClickListener {
+            selectedFace = 1
+            setCoinFace()
         }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun setCoinFace(){
+        if (binding.coinFaceImageView.visibility == VISIBLE){
+            binding.coinFaceImageView.visibility = INVISIBLE
+        }
+
+        Toast.makeText(context, "Flipping the coin...   ", Toast.LENGTH_SHORT).show()
+
+        Thread.sleep(2000)
+
+        val randValue = (0..1).random()
+
+        when(randValue){
+            0 -> binding.coinFaceImageView.setImageResource(R.drawable.dogecoin_front)
+            1 -> binding.coinFaceImageView.setImageResource(R.drawable.dogecoin_back)
+        }
+
+        if (randValue == selectedFace){
+            binding.resultTextView.text = "YOU WIN!!!"
+            Data.loggedUser!!   .winsAndDefeats[0].wins += 1
+        } else {
+            binding.resultTextView.text = "I'm Sorry! You lost, try again."
+            Data.loggedUser!!.winsAndDefeats[0].defeats += 1
+        }
+
+        binding.coinFaceImageView.visibility = VISIBLE
     }
 }
